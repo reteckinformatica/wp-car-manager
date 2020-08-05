7be4b8030df4e588acaa67b533a85a93b405c46a
reteckinformatica <reteck.webapp@gmail.com>
2020-08-05 11:39:07 -0300

    fixed style css

--- templates/single-vehicle/contact.php
+++ templates/single-vehicle/contact.php
@@ -14,13 +14,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<textarea class="wpcm-rtk-form-control" placeholder="Mensagem"></textarea>
 			<input class="button" type="submit" value="ENVIAR MENSAGEM"/>
 		</form>
-	</div>
+	</div>	
 	<div class="wpcm-rtk-whatsapp">
 		<?php
+		/*
 		if ( '' !== $phone_number && apply_filters( 'wpcm_contact_phone_link', true ) ) :
 			?>
 			<a target="_blank" href="https://wa.me/<?php echo esc_attr( $phone_number ); ?>?text=Ol%C3%A1%2C%20gostaria%20de%20receber%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20seguinte%20ve%C3%ADculo%20<?php the_permalink(); ?>"
 			class="wpcm-button wpcm-contact-button"><?php _e( '✆ Fale com o vendedor', 'wp-car-manager' ); ?></a>
-		<?php endif; ?>
+		<?php endif; */?>
+	
 	</div>
 </div>
