b0161b2deac07db00c582f94bd35e77c39cfa2e0
reteckinformatica <reteck.webapp@gmail.com>
2020-08-18 15:24:49 -0300

    Update contact whats

--- templates/single-vehicle/contact.php
+++ templates/single-vehicle/contact.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<input name="cli_email" class="wpcm-rtk-form-control" type="email" placeholder="E-mail"/>
 			<input name="cli_phone" class="wpcm-rtk-form-control" type="text" placeholder="Telefone"/>
 			<textarea name="cli_mesage" class="wpcm-rtk-form-control wpcm-rtk-textarea" placeholder="Mensagem"></textarea>
-			<button name="btn_submit_msg" class="wpcm-button" type="submit">ENVIAR MENSAGEM</button>
+			<input id="btn_rtk" name="btn_submit_msg" class="wpcm-button" type="submit" value="ENVIAR MENSAGEM" />
 		</form>
 	</div>	
 	<?php			
@@ -25,10 +25,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 		
 			$textzap = "Cliente: " . $nome . ", E-mail: " . $email  . ", Telefone: " . $cli_phone . ", Mensagem: " . $msgtext . "," . $rtk_origen;
 			$rtk_send_msg = str_replace(' ', '%20', str_replace(',','%0A', $textzap));
+
+			if ( '' !== $phone_number && apply_filters( 'wpcm_contact_phone_link', true ) ) :
 			
-		?>
-		<script type="text/javascript">
-			window.self.location.replace("<?php echo 'https://wa.me/' . esc_attr( $phone_number ) . '?text=' . $rtk_send_msg; ?>");
-		</script>
-	<?php } ?>	
+				$phoneset= 	esc_attr( $phone_number ); 
+		   
+			endif; 
+
+			$wpcm_url_add = "https://api.whatsapp.com/send?1=pt_BR&phone=$phoneset&text=$rtk_send_msg";
+		
+			?>
+			<script type="text/javascript">
+				var get_url_whats = "<?php echo $wpcm_url_add;?>";
+				var win = window.open(get_url_whats, "_blank");
+				if (win) {
+					//Browser has allowed it to be opened
+					win.focus();
+				} else {
+					//Browser has blocked it
+					alert('Por favor libere popups para este site!');
+				}
+			</script>
+			<?php
+	 	} 
+	 ?>	
 </div>
